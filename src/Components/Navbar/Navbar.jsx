dae8f790149e1993e4aacaf71f2d14951540db13
amir811537 <amirhossain.bc.75@gmail.com>
2025-04-28 16:24:11 +0600

    added cart icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -216,7 +216,7 @@ const Navbar = () => {
         <button onClick={toggleSidebar} className="absolute top-4 right-4">
           <FaTimes size={20} />
         </button>
-        <div className="p-4 pt-12 space-y-6 text-lg">
+        <div className="p-4 pt-5 space-y-6 text-lg">
           {['/', '/Products', '/service', '/contact'].map((path, idx) => (
             <NavLink
               key={idx}
@@ -238,7 +238,21 @@ const Navbar = () => {
               Login
             </button>
           )}
+    <div className='flex justify-start items-center gap-3'>
+      <p>My Cart</p>
+    <div className="text-lg">
+    <div className="relative">
+  <div className="t-0 absolute left-3">
+    <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">10</p>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="file: h-6 w-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+  </svg>
+</div>
+</div>
+    </div>
         </div>
+    
       </div>
 
       {/* Login/Sign Up Modal */}
