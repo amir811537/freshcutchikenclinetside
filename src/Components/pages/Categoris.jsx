733aa21f313b90b81f2f625477c4360bbe359a6f
amir811537 <amirhossain.bc.75@gmail.com>
2025-05-22 15:58:29 +0600

    added new hover effect

--- src/Components/pages/Categoris.jsx
+++ src/Components/pages/Categoris.jsx
@@ -75,24 +75,29 @@ const Categoris = () => {
               1024: { slidesPerView: 6 },
             }}
           >
-            {uniqueCategories.map((item, index) => (
-              <SwiperSlide key={index}>
-                <button
-                  onClick={() => handleSelectCategory(item.category)}
-                  className="w-full"
-                >
-                  <div className="flex flex-col items-center justify-center border hover:text-white dark:bg-black border-gray-300 rounded-md text-center p-2 lg:p-0 h-44 w-28 md:w-44 md:h-44 bg-white hover:bg-[#F5BC3B] shadow-sm hover:shadow-md transition">
-                    <img
-                      src={item.categoryImage}
-                      alt={item.category}
-                      loading="lazy"
-                      className="lg:w-28 lg:h-28 w-14 h-14 object-contain mb-2"
-                    />
-                    <h1 className="text-sm font-medium">{item.category}</h1>
-                  </div>
-                </button>
-              </SwiperSlide>
-            ))}
+{uniqueCategories.map((item, index) => (
+  <SwiperSlide key={index}>
+    <button
+      onClick={() => handleSelectCategory(item.category)}
+      className="w-full"
+    >
+      <div className="group relative flex flex-col items-center justify-center h-44 w-28 md:w-52 md:h-52  border border-spacing-1 lg:rounded-3xl rounded-lg border-gray-200 bg-white hover:bg-[#F5BC3B] shadow-md hover:shadow-xl transition-all gap-10  duration-300 ease-in-out overflow-hidden">
+        <div className="flex flex-col items-center justify-center text-center space-y-2">
+          <img
+            src={item.categoryImage}
+            alt={item.category}
+            loading="lazy"
+            className="w-20 h-20 md:w-28 md:h-28 object-contain transition-transform duration-500 group-hover:scale-110"
+          />
+          <h1 className="text-sm md:text-base font-medium text-gray-800 group-hover:text-white transition-colors duration-300">
+            {item.category}
+          </h1>
+        </div>
+      </div>
+    </button>
+  </SwiperSlide>
+))}
+
           </Swiper>
         )}
       </div>
